afb162b185335078e6ffd73c1c273796494bf45e
SHIVAMKUMARRAJPUT <104126320+SHIVAMKUMARRAJPUT@users.noreply.github.com>
2022-05-28 15:44:37 +0530

    Delete Numpy directory

--- Numpy/06_dict.py
+++
@@ -1,9 +0,0 @@
-sample_dict = {
-    "Topper": "Shivam",
-    "Second": "Kapil",
-    "Third": "Lovekesh",
-    "Fourth": "Prabhat"
-}
-
-sample_dict['First'] = sample_dict.pop('Fourth')
-print(sample_dict)
--- Numpy/08_dict.py
+++
@@ -1,9 +0,0 @@
-# write a program to print the sum of key vakue pairs in dictionary 
-dict = eval(input("enter the input in dictionary form"))
-print("your given dictionary is below ",dict)
-
-sumlist=[]
-for key in dict:
-    sumlist.append(key + dict[key])
-print("the sum of your dictionary's key value pairs is below")
-print(sumlist)
